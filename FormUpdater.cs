3043318cab201ab406c175b575562704c09c948e
tautcony <tautcony@gmail.com>
2018-01-02 13:28:05 +0800

    Move the updater component to a separate project.

---
+++ FormUpdater.cs
@@ -0,0 +1,81 @@
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Updater
+{
+    public partial class FormUpdater : Form
+    {
+        private readonly string _newPath;
+
+        private readonly string _exePath;
+
+        private readonly string _backupPath;
+
+        private WebClient _client;
+
+        private readonly string _baseUrl;
+
+        private readonly Version _version;
+
+        public FormUpdater(string currentProgram, Version version, string baseUrl)
+        {
+            InitializeComponent();
+            _newPath          = currentProgram + ".new";
+            _exePath          = currentProgram;
+            _backupPath       = currentProgram + ".bak";
+            labelVersion.Text = version.ToString();
+            _baseUrl = baseUrl;
+            _version = version;
+        }
+
+        private void FormUpdater_Load(object sender, EventArgs e)
+        {
+            _client = new WebClient();
+            _client.DownloadFileCompleted   += client_DownloadFileCompleted;
+            _client.DownloadProgressChanged += client_DownloadProgressChanged;
+            _client.DownloadFileAsync(new Uri($"http://{_baseUrl}/bin/{Utils.SoftwareName}.v{_version}.exe"), _newPath);
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            _client.CancelAsync();
+        }
+
+        private delegate void UpdateProgressValue(int value);
+
+        private void UpdateProgress(int value)
+        {
+            progressBarDownload.Value = value;
+        }
+
+        private void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Invoke(new UpdateProgressValue(UpdateProgress), e.ProgressPercentage);
+        }
+
+        private void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                if (File.Exists(_newPath))
+                {
+                    File.Delete(_newPath);
+                }
+                Close();
+            }
+            else
+            {
+                if (File.Exists(_backupPath))
+                {
+                    File.Delete(_backupPath);
+                }
+                File.Move(_exePath, _backupPath);
+                File.Move(_newPath, _exePath);
+                Application.Restart();
+            }
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Updater
+{
+    public static class Utils
+    {
+        private static string _softwareName;
+        public static string SoftwareName
+        {
+            set => _softwareName = value;
+            get
+            {
+                if (_softwareName == null)
+                {
+                    throw new NullReferenceException($"You must set {nameof(SoftwareName)} before using it");
+                }
+                return _softwareName;
+            }
+        }
+
+        private static Version _currentVersion;
+
+        public static Version CurrentVersion
+        {
+            set => _currentVersion = value;
+            get
+            {
+                if (_currentVersion == null)
+                {
+                    throw new NullReferenceException($"You must set {nameof(SoftwareName)} before using it");
+                }
+                return _currentVersion;
+            }
+        }
+
+        private static void OnResponse(IAsyncResult ar)
+        {
+            var versionRegex = new Regex($@"<meta\s+name\s*=\s*'{SoftwareName}'\s+content\s*=\s*'(\d+\.\d+\.\d+\.\d+)'\s*>");
+            var baseUrlRegex = new Regex(@"<meta\s+name\s*=\s*'BaseUrl'\s+content\s*=\s*'(.+)'\s*>");
+            var webRequest = (WebRequest)ar.AsyncState;
+            Stream responseStream;
+            try
+            {
+                responseStream = webRequest.EndGetResponse(ar).GetResponseStream();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(string.Format("检查更新失败, 错误信息:{0}{1}{0}请联系TC以了解详情",
+                    Environment.NewLine, exception.Message), @"Update Check Fail");
+                responseStream = null;
+            }
+            if (responseStream == null) return;
+
+            var streamReader = new StreamReader(responseStream);
+            var context = streamReader.ReadToEnd();
+            var result = versionRegex.Match(context);
+            var urlResult = baseUrlRegex.Match(context);
+            if (!result.Success || !result.Success) return;
+
+            var remoteVersion = Version.Parse(result.Groups[1].Value);
+            if (CurrentVersion >= remoteVersion)
+            {
+                return;
+            }
+            var dialogResult = MessageBox.Show(caption: @"Wow! Such Impressive", text: $"新车已发车 v{remoteVersion}，上车!",
+                                               buttons: MessageBoxButtons.YesNo, icon: MessageBoxIcon.Asterisk);
+            if (dialogResult != DialogResult.Yes) return;
+            var formUpdater = new FormUpdater(Application.ExecutablePath, remoteVersion, urlResult.Groups[1].Value);
+            formUpdater.ShowDialog();
+        }
+
+        public static void CheckUpdate()
+        {
+            if (!IsConnectInternet()) return;
+            var webRequest = (HttpWebRequest)WebRequest.Create("https://tautcony.github.io/tcupdate");
+            #if DEBUG
+            webRequest = (HttpWebRequest)WebRequest.Create("http://127.0.0.1:4000/tcupdate.html");
+            #endif
+            var userName               = Environment.UserName.ToCharArray().Aggregate("", (current, c) => current + $"{(int)c:X} ");
+            webRequest.UserAgent       = $"{userName}({Environment.OSVersion}) / {Assembly.GetExecutingAssembly().GetName().FullName}";
+            webRequest.Method          = "GET";
+            webRequest.Credentials     = CredentialCache.DefaultCredentials;
+            webRequest.KeepAlive       = false;
+            webRequest.ProtocolVersion = HttpVersion.Version10;
+            webRequest.BeginGetResponse(OnResponse, webRequest);
+        }
+
+        public static bool CheckUpdateWeekly(string program)
+        {
+            var reg = RegistryStorage.Load(@"Software\" + program, "LastCheck");
+            if (string.IsNullOrEmpty(reg))
+            {
+                RegistryStorage.Save(DateTime.Now.ToString(CultureInfo.InvariantCulture), @"Software\" + program, "LastCheck");
+                return false;
+            }
+            var lastCheckTime = DateTime.Parse(reg);
+            if (DateTime.Now - lastCheckTime > new TimeSpan(7, 0, 0, 0))
+            {
+                CheckUpdate();
+                RegistryStorage.Save(DateTime.Now.ToString(CultureInfo.InvariantCulture), @"Software\" + program, "LastCheck");
+                return true;
+            }
+            return false;
+        }
+
+        private static bool IsConnectInternet()
+        {
+            return InternetGetConnectedState(0, 0);
+        }
+
+        [DllImport("wininet.dll")]
+        private static extern bool InternetGetConnectedState(int description, int reservedValue);
+    }
+}
